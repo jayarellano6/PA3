9637a6b8b0cb8f53333cbec944848537d23c2f40
jayarellano6 <jaarellano@csumb.edu>
2019-04-14 19:12:27 -0700

    fixed small bug / changed IPs

--- legacy_router.py
+++ legacy_router.py
@@ -10,21 +10,21 @@ def myNetwork():
 
 	info( '*** Adding controller\n' )
 	info( '*** Add switches\n')
-	r1 = net.addHost('r1', cls=Node, ip='10.20.1.1/24')
+	r1 = net.addHost('r1', cls=Node, ip='10.24.1.1/24')
 	#r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
 	info( '*** Add hosts\n')
-	h1 = net.addHost('h1', cls=Host, ip='10.20.1.2/24', defaultRoute='via 10.20.1.1')
-	h2 = net.addHost('h2', cls=Host, ip='10.20.2.2/24', defaultRoute='via 10.20.2.1')
+	h1 = net.addHost('h1', cls=Host, ip='10.24.1.2/24', defaultRoute='via 10.24.1.1')
+	h2 = net.addHost('h2', cls=Host, ip='10.24.2.2/24', defaultRoute='via 10.24.2.1')
 
 	info( '*** Add links\n')
-	net.addLink(h1, r1, params2={ 'ip' : '10.20.1.1/24'})
-	net.addLink(h2, r1, params2={ 'ip' : '10.2.2.1/24'})
+	net.addLink(h1, r1, params2={ 'ip' : '10.24.1.1/24'})
+	net.addLink(h2, r1, params2={ 'ip' : '10.24.2.1/24'})
 
 	info( '*** Starting network\n')
 	net.build()
 	info('*** Starting controllers\n')
-	for controllers in net.controllers:
+	for controller in net.controllers:
 		controller.start()
 
 	CLI(net)
