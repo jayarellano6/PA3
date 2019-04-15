222ebb3a7aa4a075eff671a55069b4dbb2782c17
jayarellano6 <jaarellano@csumb.edu>
2019-04-14 19:03:18 -0700

    noticed after ending the program 'stopping 0 controllers' displays, added code to start controllers

--- legacy_router.py
+++ legacy_router.py
@@ -6,16 +6,16 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
 def myNetwork():
-	net = Mininet( topo=None, build=False, ipBase='10.44.1.0/24')
+	net = Mininet( topo=None, build=False, ipBase='10.20.1.0/24')
 
 	info( '*** Adding controller\n' )
 	info( '*** Add switches\n')
-	r1 = net.addHost('r1', cls=Node, ip='10.44.1.1/24')
+	r1 = net.addHost('r1', cls=Node, ip='10.20.1.1/24')
 	r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
 	info( '*** Add hosts\n')
-	h2 = net.addHost('h2', cls=Host, ip='10.44.1.2/24', defaultRoute=None)
-	h1 = net.addHost('h1', cls=Host, ip='10.44.1.1/24', defaultRoute=None)
+	h1 = net.addHost('h1', cls=Host, ip='10.20.1.2/24', defaultRoute='via 10.20.1.1')
+	h2 = net.addHost('h2', cls=Host, ip='10.20.2.2/24', defaultRoute='via 10.20.2.1')
 
 	info( '*** Add links\n')
 	net.addLink(h1, r1)
@@ -23,7 +23,9 @@ def myNetwork():
 
 	info( '*** Starting network\n')
 	net.build()
-
+	info('*** Starting controllers\n')
+	for controllers in net.controllers:
+		controller.start()
 
 	CLI(net)
 	net.stop()
