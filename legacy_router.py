ff850a2ae87c0d5d3501f50f9a707f3c54b2fb8b
jayarellano6 <jaarellano@csumb.edu>
2019-04-14 19:07:28 -0700

    removing r1.cmd

--- legacy_router.py
+++ legacy_router.py
@@ -11,7 +11,7 @@ def myNetwork():
 	info( '*** Adding controller\n' )
 	info( '*** Add switches\n')
 	r1 = net.addHost('r1', cls=Node, ip='10.20.1.1/24')
-	r1.cmd('sysctl -w net.ipv4.ip_forward=1')
+	#r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
 	info( '*** Add hosts\n')
 	h1 = net.addHost('h1', cls=Host, ip='10.20.1.2/24', defaultRoute='via 10.20.1.1')
