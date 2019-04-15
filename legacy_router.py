f00531646b2e2331fe3719fe03669da8b22d0492
None <None>
2019-04-14 18:51:36 -0700

    changing IP addresses again testing

--- legacy_router.py
+++ legacy_router.py
@@ -6,16 +6,16 @@ from mininet.cli import CLI
 from mininet.log import setLogLevel, info
 
 def myNetwork():
-	net = Mininet( topo=None, build=False, ipBase='10.34.0.0/16')
+	net = Mininet( topo=None, build=False, ipBase='10.44.1.0/24')
 
 	info( '*** Adding controller\n' )
 	info( '*** Add switches\n')
-	r1 = net.addHost('r1', cls=Node, ip='10.34.1.1')
+	r1 = net.addHost('r1', cls=Node, ip='10.44.1.1/24')
 	r1.cmd('sysctl -w net.ipv4.ip_forward=1')
 
 	info( '*** Add hosts\n')
-	h2 = net.addHost('h2', cls=Host, ip='10.34.1.2', defaultRoute=None)
-	h1 = net.addHost('h1', cls=Host, ip='10.34.1.1', defaultRoute=None)
+	h2 = net.addHost('h2', cls=Host, ip='10.44.1.2/24', defaultRoute=None)
+	h1 = net.addHost('h1', cls=Host, ip='10.44.1.1/24', defaultRoute=None)
 
 	info( '*** Add links\n')
 	net.addLink(h1, r1)
